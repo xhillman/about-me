50d865daa6ce714757d184a2c0320f7e8418e302
Xavier Hillman <xhillman13@gmail.com>
2022-07-14 00:40:21 -0400

    shortened variables for readability and added random num function

--- js/app.js
+++ js/app.js
@@ -4,8 +4,6 @@ let username = prompt('What is your name?');
 let userScore = 0;
 alert(`Welcome to my site ${username}! We're going to play a guessing game, okay?`);
 
-
-
 let homeState = prompt('Am I from Massachusetts?\n Yes (y) or No (n)').toLowerCase();
 if (homeState === 'y' || homeState === 'yes') {
   userScore++;
@@ -15,9 +13,7 @@ if (homeState === 'y' || homeState === 'yes') {
 } else {
   alert('I was looking for a yes or no answer but it\'s cool. On to the next question!');
 }
-console.log(userScore);
-
-
+console.log(`User Score: ${userScore}`);
 
 let myCollege = prompt('Did I attend Harvard University?\n Yes (y) or No (n)').toLowerCase();
 if (myCollege === 'y' || myCollege === 'yes') {
@@ -28,9 +24,7 @@ if (myCollege === 'y' || myCollege === 'yes') {
 } else {
   alert('I was looking for a yes or no answer but it\'s cool. On to the next question!');
 }
-console.log(userScore);
-
-
+console.log(`User Score: ${userScore}`);
 
 let myPet = prompt('Do I have a pet parrot?\n Yes (y) or No (n)').toLowerCase();
 if (myPet === 'y' || myPet === 'yes') {
@@ -41,9 +35,7 @@ if (myPet === 'y' || myPet === 'yes') {
 } else {
   alert('I was looking for a yes or no answer but it\'s cool. On to the next question!');
 }
-console.log(userScore);
-
-
+console.log(`User Score: ${userScore}`);
 
 let myJob = prompt('Am I coming directly from the aerospace industry?\n Yes (y) or No (n)').toLowerCase();
 if (myJob === 'y' || myJob === 'yes') {
@@ -54,53 +46,46 @@ if (myJob === 'y' || myJob === 'yes') {
 } else {
   alert('I was looking for a yes or no answer but it\'s cool. On to the next question!');
 }
-console.log(userScore);
-
-
+console.log(`User Score: ${userScore}`);
 
 let pastCoding = prompt('Did I know any coding before I started Code 201?\n Yes (y) or No (n)').toLowerCase();
 if (pastCoding === 'y' || pastCoding === 'yes') {
   userScore++;
-  alert('Correct! I self taught for 6 months and then took Code 102.');
+  alert('Correct! I self taught for a few months and then took Code 102.');
 } else if (pastCoding === 'n' || pastCoding === 'no') {
   alert('You expect too much from me. I did some self-teaching and took Code 102!');
 } else {
   alert('I was looking for a yes or no answer but it\'s cool. On to the next question!');
 }
-console.log(userScore);
+console.log(`User Score: ${userScore}`);
 
-
-
-let myNum = 13;
+let myNum = Math.floor(Math.random() * 25);
+console.log(`Random Number: ${myNum}`);
 let numGuesses = 1;
-let userNumGuess = prompt('Guess a number between 1 and 25! \nI\'ll give you 4 tries.');
-if (userNumGuess === myNum) {
+let userNum = prompt('Guess a number between 1 and 25! \nI\'ll give you 4 tries.');
+if (userNum === myNum) {
   userScore++;
-  alert(`You got it! My number was ${userNumGuess}!`);
+  alert(`You got it! My number was ${userNum}!`);
 } else {
-  while (userNumGuess !== myNum && numGuesses < 4) {
-    if (userNumGuess > myNum) {
-      alert(`Too high! Try a number lower than ${userNumGuess}`);
-      userNumGuess = prompt(`Guess a number from 1 to 25. You have ${4 - numGuesses} left!`);
+  while (userNum !== myNum && numGuesses < 4) {
+    if (userNum > myNum) {
       numGuesses++;
-    } else if (userNumGuess < myNum) {
-      alert(`Too low! Try a number higher than ${userNumGuess}`);
-      userNumGuess = prompt(`Guess a number from 1 to 25. You have ${4 - numGuesses} left!`);
+      userNum = prompt(`Too high!\nYou have ${4 - numGuesses} left!`);
+    } else if (userNum < myNum) {
+      userNum = prompt(`Too Low!\nYou have ${4 - numGuesses} left!`);
       numGuesses++;
     } else {
-      userNumGuess = myNum;
+      userNum = myNum;
       userScore++;
-      alert(`You got it! My number was ${userNumGuess}!`);
+      alert(`You got it! My number was ${userNum}!`);
       break;
     }
   }
 }
-if (userNumGuess !== myNum) {
+if (userNum !== myNum) {
   alert(`The number was ${myNum}!`);
 }
-console.log(userScore);
-
-
+console.log(`User Score: ${userScore}`);
 
 let favColors = ['black', 'green', 'blue'];
 let numColorGuesses = 1;
@@ -118,8 +103,6 @@ while (numColorGuesses < 6) {
 if (userColorGuess !== favColors[0] && userColorGuess !== favColors[1] && userColorGuess !== favColors[2]) {
   alert(`You tried your best! My three favorite colors are ${favColors[0]}, ${favColors[1]} and ${favColors[2]}!`);
 }
-console.log(userScore);
+console.log(`User Score: ${userScore}`);
 
-
-
-alert(`Thanks for playing ${username}! You got ${userScore} out of 7 questions right! Maybe next time you can ask me some questions!`);
+alert(`Thanks for playing ${username}! You got ${userScore} out of 7 questions correct!\nMaybe next time you can ask me some questions!`);
